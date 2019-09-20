e24ead293731072c7fc2919ba440dfae3d617f64
mesadesoluciones <mesadeservicio@cesde.edu.co>
2019-09-19 20:26:46 -0500

    3 idiomas

--- nombres.js
+++ nombres.js
@@ -17,6 +17,7 @@ stdin.addListener("data", function(d) {
         
         for (let index = 0; index <= 0; index++) {
             tabla += "hola " + dato + "\n";
+            tabla += "Kon'nichiwa " + dato + "\n";
             tabla += "hello " + dato;
         }
         
